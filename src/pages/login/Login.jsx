9a17ee95acce0d21e36d39904f2fd9a7296e73f8
sinsorn07 <sinsorn@gmail.com>
2024-11-02 18:58:39 +0700

    I adjested Login and Register Pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -6,11 +6,10 @@ const Login = () => {
         <div className="login">
             <div className="card">
                 <div className="left">
-                    <h1>Hello World</h1>
+                    <h1>Meetro Social</h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-                        alias totam numquam ipsa exercitationem dignissimos, error nam,
-                        consequatur.
+                        "Capture the Vibe, 
+                        <br/>Relive the Moments"
                     </p>
                     <span>Don't you have an account?</span>
                     <Link to="/register">
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -8,9 +8,8 @@ const Register = () => {
                 <div className="left">
                     <h1>Meetro Social</h1>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-                        alias totam numquam ipsa exercitationem dignissimos, error nam,
-                        consequatur.
+                        Get started by creating an account and explore a world where every gathering is memorable. 
+                        Share photos, join conversations, and keep the good times going.
                     </p>
                     <span>Do you have an account?</span>
                     <Link to="/login">
