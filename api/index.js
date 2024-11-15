215b34ff70a16ba651f03c53bbe06b8e3301d3b4
quacksy04 <Jeejee3599@gmail.com>
2024-11-15 16:31:49 +0700

    setup file

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import eventRoutes from "./routes/events.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//middlewares
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+  app.use(express.json());//receiving json objects(data of users) from login/register pages
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
+  app.use(cookieParser());
+  
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/events", eventRoutes);
+
+app.listen(8800, ()=> {
+    console.log("API working");
+});
+
---
+++ api/routes/events.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {  } from "../controllers/event.js";
+
+
+const router = express.Router()
+
+router.get("",)
+
+export default router
