97658a45d3025477e5935e9abf4d4b14933ec8d5
quacksy04 <Jeejee3599@gmail.com>
2024-11-20 23:46:15 +0700

    updated

--- api/routes/events.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {  } from "../controllers/event.js";
-
-
-const router = express.Router()
-
-router.get("",)
-
-export default router
--- api/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {  } from "../controllers/post.js";
-
-
-const router = express.Router()
-
-router.get("",)
-
-export default router
