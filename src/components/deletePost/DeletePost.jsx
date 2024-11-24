238ae174256742f87e76eb5b44da2d2eb8a01195
NattyHam <160451333+NattyHam@users.noreply.github.com>
2024-11-24 20:05:12 +0700

    Update DeletePost.jsx button

--- src/components/deletePost/DeletePost.jsx
+++ src/components/deletePost/DeletePost.jsx
@@ -12,17 +12,17 @@ const DeleteEvent = ({ isOpen, onClose, onDelete }) => {
           Are you sure you want to delete this post?
         </h2>
         <div className="flex justify-center space-x-4">
-          {/* Delete Button */}
+          {/* Cancel Button */}
           <button
             onClick={onClose}
-            className="px-4 py-2 bg-gray-400 text-[#EEEEEE] rounded-md hover:bg-gray-500 focus:ring-4 focus:ring-[#508C9B]"
+            className="px-4 py-2 bg-gray-400 text-[#EEEEEE] rounded-md hover:bg-gray-500 focus:ring-4 focus:ring-[#508C9B] w-32"
           >
             Cancel
           </button>
-          {/* Cancel Button */}
+          {/* Delete Button */}
           <button
             onClick={onDelete}
-            className="px-4 py-2 bg-[#508C9B] text-[#EEEEEE] rounded-md hover:bg-[#134B70] focus:ring-4 focus:ring-[#134B70]"
+            className="px-4 py-2 bg-[#508C9B] text-[#EEEEEE] rounded-md hover:bg-[#134B70] focus:ring-4 focus:ring-[#134B70] w-32"
           >
             Delete
           </button>
